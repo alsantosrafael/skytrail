b705562d7a262cb0864adaff51cb1c01e9b5388c
Rafael Almeida <rafael.profeng@gmail.com>
2025-09-17 16:41:59 -0300

    chore: Create boilerplate for project

---
+++ src/main/java/com/pilgrim/skytrail/SkyTrailApplication.java
@@ -0,0 +1,13 @@
+package com.pilgrim.skytrail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyTrailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyTrailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pilgrim/skytrail/SkyTrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pilgrim.skytrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyTrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
