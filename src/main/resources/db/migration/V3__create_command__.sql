e4327cd32d36b4c77c3089be3503b026f71da658
Rafael Almeida <rafael.profeng@gmail.com>
2025-09-17 18:02:55 -0300

    fix: Adjust command migration

--- src/main/resources/db/migration/V3__create_command__.sql
+++ src/main/resources/db/migration/V3__create_command__.sql
@@ -1,6 +1,7 @@
 CREATE TABLE command (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    type VARCHAR(50) NOT NULL,
+    type VARCHAR(100) NOT NULL,
     protocol VARCHAR(1000) NOT NULL,
-    created_at TIMESTAMP DEFAULT now()
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
 );
