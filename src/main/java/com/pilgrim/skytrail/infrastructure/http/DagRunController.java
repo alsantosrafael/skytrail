255782ab7acc8250daa7ef9cce99d9ca20d755b9
Rafael Almeida <rafael.profeng@gmail.com>
2025-09-17 19:19:32 -0300

    refactor: Isolate responsibilities for Dag Controller and DagRun Controller
    # Conflicts:
    #	src/main/java/com/pilgrim/skytrail/infrastructure/http/DagRunController.java

--- src/main/java/com/pilgrim/skytrail/infrastructure/http/DagRunController.java
+++ src/main/java/com/pilgrim/skytrail/infrastructure/http/DagRunController.java
@@ -1,15 +1,12 @@
 package com.pilgrim.skytrail.infrastructure.http;
 
 import com.pilgrim.skytrail.application.services.DagService;
-import com.pilgrim.skytrail.domain.dtos.FetchDagResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
